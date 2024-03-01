009103494cf0d971b0bf1008589950144c1143a5
Amoong <moo1323@naver.com>
2024-03-02 07:30:20 +0900

    Init

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] InputAction movement;
+    void Start()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+
+
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThrow);
+
+        // float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);
+    }
+}
