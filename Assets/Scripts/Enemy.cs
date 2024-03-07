820d0eee5ec64afe1991c0f662943738d08606b3
Amoong <moo1323@naver.com>
2024-03-07 17:09:49 +0900

    Add enemy explosion

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
 
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log("Destory");
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField] float timeTillDestroy = 1f;
+
+    void Start()
+    {
+        Destroy(gameObject, timeTillDestroy);
+    }
+
+}
