4f744a0c9a4cca302a31298c2ea505ad325f7b87
Amoong <moo1323@naver.com>
2024-03-08 18:14:59 +0900

    Add score system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,34 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        // 객체들 중 하나를 찾아서 반환
+        // 리소스가 많이 드는 작업
+        // Update 메소드에선 사용 X
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+    void KillEnemy()
+    {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
-
-        Destroy(this.gameObject);
+        Destroy(gameObject);
     }
+
+
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+
+    public void IncreaseScore(int amountToIncrease)
+    {
+        score += amountToIncrease;
+        Debug.Log($"Score is now: {score}");
+    }
+
+}
