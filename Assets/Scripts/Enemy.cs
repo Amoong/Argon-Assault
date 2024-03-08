b92a15b175deed984c4874b92dacdc85e2ae96e3
Amoong <moo1323@naver.com>
2024-03-08 20:22:01 +0900

    Add health to Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] int hitPoint = 3;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
 
     ScoreBoard scoreBoard;
+    int originHealth = 1;
 
     void Start()
     {
@@ -14,17 +17,31 @@ public class Enemy : MonoBehaviour
         // 리소스가 많이 드는 작업
         // Update 메소드에선 사용 X
         scoreBoard = FindObjectOfType<ScoreBoard>();
+
+        originHealth = hitPoint;
     }
 
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
     }
 
     void ProcessHit()
     {
         scoreBoard.IncreaseScore(scorePerHit);
+
+        float healthPercent = (float)hitPoint / originHealth;
+
+        Material material = GetComponent<MeshRenderer>().material;
+        material.color = new Color(1, healthPercent, healthPercent);
+
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+
+        if (--hitPoint < 1)
+        {
+            KillEnemy();
+        }
     }
     void KillEnemy()
     {
